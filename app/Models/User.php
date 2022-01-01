97ca9029617acaebaf2952076bb2c5e7819ec521
ZaydSK <z.a.skaf@gmail.com>
2022-01-01 19:33:58 +0200

    added has many comments relation to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,4 +60,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Like::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
